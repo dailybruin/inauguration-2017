289aa481c1bd876824f3a76760924f0524b2d5eb
Anthony Lai <yukuan.anthony.lai@gmail.com>
2017-01-16 19:26:04 -0800

    Add filtering to handlebar

--- js/main.js
+++ js/main.js
@@ -2,29 +2,29 @@ $(document).ready(function() {
     "use strict";
     console.log("main.js successfully loaded");
     populateItems();
+    changeActive();
 });
 
+var ITEMS_DATA;
+
 // functions
+function displayItems(data) {
+    var source = $("#item-template").html();
+    var template = Handlebars.compile(source);
+    var context = data;
+    var html = template(context);
+
+    $(".item-container").empty();
+    $(".item-container").append(html);
+}
+
 function populateItems() {
     $.ajax({
         dataType: "json",
         url: "https://spreadsheets.google.com/feeds/list/1eigBGV50ScQkGjgPTuH9WO8tYUmN2UpdM-XTFzh-lyQ/od6/public/values?alt=json",
         success: function(data) {
-            data = data.feed.entry
-            console.log(data);
-
-            var source = $("#item-template").html();
-            var template = Handlebars.compile(source);
-            var context = data;
-            var html = template(context);
-            $(".item-container").append(html);
-
-
-            // var template = $('#items').html();
-            // var templateScript = Handlebars.compile(template);
-            // var context = data;
-            // var html = templateScript(context);
-            // $("#handlebars-content").append(html);
+            ITEMS_DATA = data.feed.entry
+            displayItems(ITEMS_DATA);
         },
         error: function(e) {
             console.log("fail");
@@ -32,3 +32,40 @@ function populateItems() {
         }
     });
 }
+
+function filterData(category) {
+    if (category === "All") {
+        displayItems(ITEMS_DATA);
+    } else {
+        var filteredData = [];
+        Object.keys(ITEMS_DATA).forEach(function(key, idx) {
+            if (ITEMS_DATA[key].gsx$category.$t === category) {
+                var temp = {};
+                temp[key] = ITEMS_DATA[key];
+                filteredData.push(ITEMS_DATA[key]);
+            }
+        });
+        displayItems(filteredData);
+    }
+}
+
+function changeActive() {
+    $(".main .navbar ul li a").toArray().forEach(function (element) {
+        $(element).click(function () {
+            console.log("clicked");
+
+            if ($(element).parent().is(":nth-child(1)")) {
+                filterData("All");
+            } else if ($(element).parent().is(":nth-child(3)")) {
+                filterData("News");
+            } else if ($(element).parent().is(":nth-child(5)")) {
+                filterData("Opinion");
+            } else if ($(element).parent().is(":nth-child(7)")) {
+                filterData("Analysis");
+            } else if ($(element).parent().is(":nth-child(9)")) {
+                filterData("Community");
+            }
+        });
+    });
+}
+
